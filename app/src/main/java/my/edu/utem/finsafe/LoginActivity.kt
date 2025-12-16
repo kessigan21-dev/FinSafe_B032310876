5599808851bd93888ce1e66f2b3337e2680e7f99
Syafiq Sinwan <syafiq.sinwan@gmail.com>
2025-12-16 21:59:33 +0800

    Added LoginActivity and its XML.

--- app/src/main/java/my/edu/utem/finsafe/LoginActivity.kt
+++ app/src/main/java/my/edu/utem/finsafe/LoginActivity.kt
@@ -1,20 +1,46 @@
 package my.edu.utem.finsafe
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var usernameInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        usernameInput = findViewById(R.id.etUsername)
+        passwordInput = findViewById(R.id.etPassword)
+        loginButton = findViewById(R.id.btnLogin)
+
+        checkLoginStatus()
+
+        loginButton.setOnClickListener {
+            val user = usernameInput.text.toString()
+            val pass = passwordInput.text.toString()
+            performLogin(user, pass)
         }
+    }
+
+    private fun checkLoginStatus() {
+        // TODO 1: Check SharedPreferences. If "isLoggedIn" is true, navigate directly to DashboardActivity
+    }
+
+    private fun performLogin(user: String, pass: String) {
+        // TODO 2: Implement logic
+        // Standard User -> User: "std", Pass: "1234"
+        // Premium User -> User: "prm", Pass: "1234"
+
+        // TODO 3: On success, save "isLoggedIn" boolean and "userType" string to SharedPreferences
+        // TODO 4: Navigate to DashboardActivity
     }
 }
